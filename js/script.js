3c6496775d079653344f5f5c98ba42627a1ebdcc
Michael <sound.plus.fury@gmail.com>
2019-06-11 11:28:29 -0700

    Day 2 updates

--- js/script.js
+++ js/script.js
@@ -7,21 +7,25 @@ var lives = ["5"];
 var totals = 0;
 var health = 0;
 var healthEl;
+var flavorTxtEl;
 var name;
 var nameEl;
 var dice;
 var img1 = "img/monster1";
-var img2 = "img/deadmonster";
+var img2 = "img/monster1dead";
 var dungeon = [ 
 		{	health: 10,
-				name: "Medium",
+				name: "Medium Monster",
+				flavorTxt: "Likes long walks on the beach and eating your face...",
 		},
 		{	health: 5, 
-				name: "Easy",
+				name: "Easy Orc",
+				flavorTxt: "Used to believe in himself, but then took an arrow to the knee.",
 		},
 		{
 		health: 16,
-			name: "Hard",
+			name: "Hard Hellion",
+			flavorTxt: "Large, hard, and in charge."
 		},
 ];
 
@@ -36,22 +40,43 @@ function roll() {
 	totals = rnd;
 	if (rnd >= health) {
 		document.getElementById("text").textContent = ("You win!");
-		//lives down?
+		demon.src = 'img/monster1dead.jpg';
+		
+		//lives down/up? 
 	} else { 
 		document.getElementById("text").textContent = ("You lose...this time!"); 
-		//lives up?
+		//lives up? if no lives, disable
+		//display game over?
 	} 
 };	
 
-//Gets monster
+//Gets monster, starts game
 function start() {
 	healthEl.textContent=dungeon[0].health;
 	health=dungeon[0].health;
+	// flavorTxtEl.textContent=dungeon[0].flavorTxt;
+	// text=dungeon[0].flavorTxt;
 	document.getElementById("text").textContent = ("Good luck..."); 
+	document.getElementById("monsterBio").textContent = (dungeon[0].flavorTxt);
 	//store health/hp in global
 	//reset image to live monster
 	
 };
+
+function reset() {
+	document.getElementById("total").textContent = ("0");
+	document.getElementById("hp").textContent = ("0");
+	document.getElementById("text").textContent = ("Welcome mortal! Push 'Start' to Begin!");
+	document.getElementById("monsterBio").textContent = ("Who shall we slay today?");
+	demon.src = 'img/monster1.jpg';
+};
+
+
+//Resets game state
+
+	//lives restored
+	//re-activate roll button
+
 
 // function checkForLoss() {
 // 	//if turns = loss, else keep
@@ -63,8 +88,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	rollButton.addEventListener("click", roll)
 	demon=document.getElementById("demon",)
 	healthEl=document.getElementById("hp",)
+	// flavorTxtEl=document.getElementById("text",)
 	startButton = document.getElementById("startButton")
 	startButton.addEventListener("click", start)
+	resetButton=document.getElementById("resetButton")
+	resetButton.addEventListener("click", reset)
 	
 
 } );
@@ -79,42 +107,6 @@ Hard Hat Area
 
 
 
-var hp = monster life
-
-if totals > hp then WIN
-
-
-
-
-
-
-
-
-
-this works for sure:
-
-
-function roll() {
-	var rnd = Math.floor(Math.random() * 20);
-	document.getElementById('total').textContent = rnd;
-	totals = rnd;
-};
-
-
-
-
-
-
-buttons:
-
-rollButton 
-//roll the dice
-
-button3
-//Start game
-
-button4
-//reset game
 
 
 
